c2ca64ac51f9915e4c55cd93455c342431f7399d
madonghwi <ehd0831@naver.com>
2023-05-24 18:10:11 +0900

    signup 추가

---
+++ static/js/users/api.js
@@ -0,0 +1,42 @@
+const frontend_base_url = "http://127.0.0.1:5500"
+const backend_base_url = "http://127.0.0.1:8000"
+
+// 회원가입
+async function handleSignin() {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const passwordCheck = document.getElementById("password-check").value
+
+    const response = await fetch(`${backend_base_url}/users/signup/`, {
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "email": email,
+            "password1": password,
+            "password2": passwordCheck,
+        })
+    })
+
+    return response
+}
+
+// 로그인
+async function handleLogin() {
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+    const response = await fetch(`${backend_base_url}/users/login/`, {
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "email": email,
+            "password": password
+        })
+    })
+
+    return response
+}
---
+++ static/js/users/signup.js
@@ -0,0 +1,13 @@
+async function handleSigninButton() {
+    const response = await handleSignin();
+    if (response.status == 201) {
+        alert("회원가입 완료")
+        window.location.replace(`${frontend_base_url}/users/login.html`)
+    } else if (response.status == 400) {
+        alert("값을 제대로 입력해 주십시오")
+    } else {
+        alert(`${response.statusText}`)
+    }
+}
+
+checkLogin();
